5ddc1c131fd9ef0d90e1c7eadfd741976f8454a7
SitnicCristina <sitnic.cristina@gmail.com>
2021-03-19 19:27:31 +0200

    Homework for 18.03

---
+++ src/md/tekwill/exercise1803/DisplayPattern.java
@@ -0,0 +1,13 @@
+package md.tekwill.exercise1803;
+
+public class DisplayPattern {
+    public static void main(String[] arg){
+
+        System.out.println(" x                    x");
+        System.out.println(" x                    x");
+        System.out.println(" x     xxxxxxxxxx     x");
+        System.out.println(" xxxxx x        x xxxxx");
+        System.out.println(" x   x x        x x   x");
+        System.out.println(" x   x x        x x   x");
+    }
+}
---
+++ src/md/tekwill/exercise1803/MovieQuoteDetails.java
@@ -0,0 +1,12 @@
+package md.tekwill.exercise1803;
+
+public class MovieQuoteDetails {
+    public static void main(String[] arg){
+       int year = 2001;
+       String film_name = "Artificial Intelligence ";
+       String quote = "'You are a real boy. At least as real as I have ever made one.' ";
+       String who_said = "Professor Hobby";
+
+       System.out.println(who_said +": " + quote + film_name + year );
+    }
+}
